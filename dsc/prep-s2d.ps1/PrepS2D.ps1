8549c3cb15bbad5e383ef1361886e6e554fdc2a9
Keith Mayer <keith@keithmayer.com>
2017-01-03 19:57:35 -0500

    Clean up Prep s2d DSC resource

--- dsc/prep-s2d.ps1/PrepS2D.ps1
+++ dsc/prep-s2d.ps1/PrepS2D.ps1
@@ -26,20 +26,7 @@ configuration PrepS2D
 
     Node localhost
     {
-<#
-        xWaitforDisk Disk2
-        {
-             DiskNumber = 2
-             RetryIntervalSec =$RetryIntervalSec
-             RetryCount = $RetryCount
-        }
 
-        cDiskNoRestart SRLogDisk
-        {
-            DiskNumber = 2
-            DriveLetter = "F"
-        }
-#>
         WindowsFeature FC
         {
             Name = "Failover-Clustering"
